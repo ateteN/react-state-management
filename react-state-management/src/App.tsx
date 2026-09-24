50b8d87f696cea96909940ca6fe24df23ed9eba1
atete <n.atete@alustudent.com>
2026-09-24 16:43:03 +0200

    feat: integrate TaskManager component and add styling for task management

--- react-state-management/src/App.tsx
+++ react-state-management/src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "./context/ThemeContext";
 import Navbar from "./components/Navbar";
+import TaskManager from "./components/TaskManager";
 
 function App() {
   return (
     <ThemeProvider>
       <Navbar />
+      <TaskManager />
     </ThemeProvider>
   );
 }
